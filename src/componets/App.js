1abd2867758531388e06de93c905b330e20c55d9
Yazima <yazima04200@gmail.com>
2022-09-27 17:50:46 +0900

    first commit

---
+++ src/componets/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// Languageコンポーネントをインポートしてください
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>言語一覧</h1>
+        <div className="language">
+          {/* Languageコンポーネントを呼び出してください */}
+          
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
